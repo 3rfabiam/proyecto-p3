6b3fc3d871dcdfd39f78264ab34fadcfb653398d
Erfabiam <fabian.araujo131@gmail.com>
2022-11-19 10:38:49 -0400

    todo fino para empezar

--- proyectop3.cpp
+++ proyectop3.cpp
@@ -3,14 +3,21 @@
 #include <stdlib.h>
 #include <algorithm>
 
-
 using namespace std;
 
-
-
 int main(int argc, char** argv) {
-
-
+	char res;
+	int i=1;
+	while(i==1){
+		cout<<"eres un bachaco?"<<endl;
+		cin>>res;
+		if (res!='y')
+		{
+			i=0;
+		}
+		system("cls");
+		
+	}
 
 	return 0;
 }
